675ab68d0e8e72256fee4ac7459bcb8bfa123f45
xuzheng465 <xuzheng465@gmail.com>
2022-05-27 11:40:30 +0800

    add sort feature

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,9 @@
 package data
 
-import "github.com/xuzheng465/greenlight/internal/validator"
+import (
+	"github.com/xuzheng465/greenlight/internal/validator"
+	"strings"
+)
 
 type Filters struct {
 	Page         int
@@ -15,4 +18,20 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize > 0, "page_size", "must be greater than 0")
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 	v.Check(validator.In(f.Sort, f.SortSafeList...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafeList {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
